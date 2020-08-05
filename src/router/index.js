4238a279bccc7a16d836dd8c9f14f0667da93fb3
zhangdy <zhangdy@cadg.cn>
2020-08-05 09:16:36 +0800

    feat(fix):

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Map from '@/components/Map'
-import SceneView from '@/components/SceneView'
 
 Vue.use(Router)
 
@@ -12,14 +10,6 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }, {
-      path: '/map',
-      name: 'Map',
-      component: Map
-    }, {
-      path: '/sceneView',
-      name: 'SceneView',
-      component: SceneView
     }
   ]
 })
